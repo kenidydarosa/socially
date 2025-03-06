5a8c2e7b8ed28e9552ce4387ffaf6655654da178
kenidydarosa <kenidyy@hotmail.com>
2025-03-05 21:53:28 -0300

    Feat: Add user in table db

---
+++ src/actions/user.action.ts
@@ -0,0 +1,34 @@
+'use server';
+
+import prisma from '@/lib/prisma';
+import { auth, currentUser } from '@clerk/nextjs/server';
+
+export const syncUser = async () => {
+  try {
+    const { userId } = await auth();
+    const user = await currentUser();
+
+    if (!userId || !user) return;
+
+    // verifica se o usuário existe
+    const existingUser = await prisma.user.findUnique({
+      where: { clerkId: userId },
+    });
+
+    if (existingUser) return existingUser;
+
+    const dbUser = await prisma.user.create({
+      data: {
+        clerkId: userId,
+        name: `${user.firstName || ''} ${user.lastName || ''}`,
+        username: user.username ?? user.emailAddresses[0].emailAddress.split('@')[0],
+        email: user.emailAddresses[0].emailAddress,
+        Image: user.imageUrl,
+      },
+    });
+
+    return dbUser;
+  } catch (error) {
+    console.log('Error in syncUser', error);
+  }
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import { Button } from '@/components/ui/button';
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
-
 export default function Home() {
   return (
     <div className='m-4'>
